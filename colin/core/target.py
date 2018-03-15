d8028dac6c1739afdae5df91a0b7937a6d8793a4
lachmanfrantisek <flachman@redhat.com>
2018-03-15 10:58:18 +0100

    Correct naming of target type
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- colin/core/target.py
+++ colin/core/target.py
@@ -17,12 +17,12 @@ class Target(object):
 
 class TargetType(enum.Enum):
     DOCKERFILE = 0
-    CONTAINER_IMAGE = 1
-    IMAGE = 2
+    CONTAINER = 1
+    CONTAINER_IMAGE = 2
 
 
 def is_compatible(target_type, check_class, severity, tags):
     # TODO take severity and tags into consideration
     return (target_type == TargetType.DOCKERFILE and isinstance(check_class, DockerfileCheck)) \
-           or (target_type == TargetType.CONTAINER_IMAGE and isinstance(check_class, ContainerCheck)) \
-           or (target_type == TargetType.IMAGE and isinstance(check_class, ImageCheck))
+           or (target_type == TargetType.CONTAINER and isinstance(check_class, ContainerCheck)) \
+           or (target_type == TargetType.CONTAINER_IMAGE and isinstance(check_class, ImageCheck))
