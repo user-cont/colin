290c9fcd3842995987ea247ac0034e6c8b974ad8
lachmanfrantisek <flachman@redhat.com>
2018-03-17 21:04:06 +0100

    Use sem-ver
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='colin',
-    version='0.1',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
         'Click',
