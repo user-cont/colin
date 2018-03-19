9ebf9cb4f1c35a3f8c76e708e6e2f0a935f022cd
lachmanfrantisek <flachman@redhat.com>
2018-03-19 15:15:10 +0100

    Move config files to /config
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ setup(
         'six',
         'conu'
     ],
-    entry_points='''
-        [console_scripts]
-        colin=colin.cli.colin:cli
-    ''',
-    data_files=[("/usr/share/colin/", ["colin/core/config/default.json",
-                                       "colin/core/config/fedora.json",
-                                       "colin/core/config/rhel.json"])],
+    entry_points={
+        'console_scripts': [
+            'colin = colin.cli.colin:cli',
+        ],
+    },
+    data_files=[("/usr/share/colin/", ["config/default.json",
+                                       "config/fedora.json",
+                                       "config/rhel.json"])],
 )
