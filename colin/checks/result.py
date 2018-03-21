15e6509b45bfe69a0668e131ed91f2bd07bf9239
lachmanfrantisek <flachman@redhat.com>
2018-03-21 14:59:03 +0100

    Stream check results
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- colin/checks/result.py
+++ colin/checks/result.py
@@ -56,4 +56,59 @@ class ImageCheckResult(AbstractResult):
 class CheckResults(object):
 
     def __init__(self, results):
-        self.results = results
+        self._results = results
+        self._generated = False
+        self._generated_result = {}
+
+    @property
+    def _dict_of_results(self):
+        result_json = {}
+        for group, results in self.results:
+            result_list = []
+            for r in results:
+                result_list.append({
+                    'name': r.check_name,
+                    'ok': r.ok,
+                    'status': r.status,
+                    'description': r.description,
+                    'message': r.message,
+                    'reference_url': r.reference_url,
+                    'severity': r.severity
+                })
+            result_json[group] = result_list
+        return result_json
+
+    @property
+    def json(self):
+        return json.dumps(self._dict_of_results, indent=4)
+
+    @property
+    def all_results(self):
+        result = []
+        for _, checks in self.results:
+            result += checks
+        return result
+
+    def save_json_to_file(self, file):
+        json.dump(obj=self._dict_of_results,
+                  fp=file,
+                  indent=4)
+
+    @property
+    def results(self):
+        if self._generated:
+            return iteritems(self._generated_result)
+        else:
+            return self._group_generator()
+
+    def _group_generator(self):
+        for group, group_result in self._results:
+            self._generated_result.setdefault(group, [])
+            yield group, self._result_generator(group=group,
+                                                results=group_result)
+        self._generated = True
+
+    def _result_generator(self, group, results):
+        for r in results:
+            self._generated_result[group].append(r)
+            yield r
