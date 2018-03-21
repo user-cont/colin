a41a49c2cce8b9932a2da5729cd6d63d7b4a2a26
lachmanfrantisek <flachman@redhat.com>
2018-03-21 15:02:23 +0100

    Better cli, allow export to json
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- colin/cli/colin.py
+++ colin/cli/colin.py
@@ -1,5 +1,7 @@
 import click as click
+from six import iteritems
 
+from ..core.constant import COLOURS
 from ..core.exceptions import ColinException
 from ..core.colin import run
 
@@ -10,21 +12,31 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.argument('target', type=click.STRING)
 @click.option('--config', '-c', type=click.STRING,
               help="Configuration name.")
-def cli(target, config):
+@click.option('--json', type=click.File(mode='w'), help="File to save the output as json to.")
+def cli(target, config, json):
     try:
         results = run(name_of_target=target,
                       config_name=config)
 
-        for r in results.results:
-            click.secho(str(r), err=r.ok, fg='green' if r.ok else 'red')
-            if not r.ok:
-                click.secho("   -> {}\n"
-                            "   -> {}\n"
-                            "   -> {}".format(r.message,
-                                              r.description,
-                                              r.reference_url),
-                            fg='red')
-            click.echo()
+        for group, check_results in results.results:
+
+            group_title_printed = False
+            for r in check_results:
+
+                if not group_title_printed:
+                    click.echo("{}:".format(group.upper()))
+                    group_title_printed = True
+
+                click.secho(str(r), err=r.ok, fg=COLOURS[r.status])
+                if not r.ok:
+                    click.secho("   -> {}\n"
+                                "   -> {}\n"
+                                "   -> {}".format(r.message,
+                                                  r.description,
+                                                  r.reference_url),
+                                fg=COLOURS[r.status])
+        if json:
+            results.save_json_to_file(file=json)
 
     except ColinException as ex:
         raise click.ClickException(str(ex))
