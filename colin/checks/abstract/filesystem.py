afe3eb3571587389e377963b608c97e382421eb0
lachmanfrantisek <flachman@redhat.com>
2018-03-22 11:43:45 +0100

    Add better handling of filesystem checks
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- colin/checks/abstract/filesystem.py
+++ colin/checks/abstract/filesystem.py
@@ -1,6 +1,7 @@
 from .containers import ContainerCheck
 from .images import ImageCheck
 from ..result import CheckResult
+from ...core.exceptions import ColinException
 
 
 class FileSystemCheck(ContainerCheck, ImageCheck):
@@ -11,21 +12,32 @@ class FileSystemCheck(ContainerCheck, ImageCheck):
         self.all_must_be_present = all_must_be_present
 
     def check(self, target):
-        with target.instance.mount() as fs:
-            passed = True
-            logs = []
-            for f in self.files:
-                f_present = fs.file_is_present(f)
-                if self.all_must_be_present:
-                    passed = f_present and passed
-                else:
-                    passed = f_present or passed
-                logs.append("File '{}' {}present."
-                            .format(f, "not " if f_present else ""))
-            return CheckResult(ok=passed,
-                               severity=self.severity,
-                               description=self.description,
-                               message=self.message,
-                               reference_url=self.reference_url,
-                               check_name=self.name,
-                               logs=logs)
+        try:
+            with target.instance.mount() as fs:
+                passed = self.all_must_be_present
+
+                logs = []
+                for f in self.files:
+                    try:
+                        f_present = fs.file_is_present(f)
+                        logs.append("File '{}' is {}present."
+                                    .format(f, "" if f_present else "not "))
+                    except IOError as ex:
+                        f_present = False
+                        logs.append("Error: {}".format(str(ex)))
+
+                    if self.all_must_be_present:
+                        passed = f_present and passed
+                    else:
+                        passed = f_present or passed
+
+                return CheckResult(ok=passed,
+                                   severity=self.severity,
+                                   description=self.description,
+                                   message=self.message,
+                                   reference_url=self.reference_url,
+                                   check_name=self.name,
+                                   logs=logs)
+        except Exception as ex:
+            raise ColinException("Problem with mounting filesystem with atomic. ({})"
+                                 .format(str(ex)))
