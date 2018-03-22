7d63b642b0c1f6af5eff01e3aefb916f56798168
lachmanfrantisek <flachman@redhat.com>
2018-03-22 13:07:14 +0100

    Split helpfile check for fedora/rhel
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- colin/checks/best_practices/help_file_or_readme.py
+++ colin/checks/best_practices/help_file_or_readme.py
@@ -1,15 +1,15 @@
 from colin.checks.abstract.filesystem import FileSystemCheck
 
 
-class HelpFileCheck(FileSystemCheck):
+class HelpFileOrReadmeCheck(FileSystemCheck):
 
     def __init__(self):
-        super().__init__(name="help_file_required",
+        super().__init__(name="help_file_or_readme_required",
                          message="The 'helpfile' has to be provided.",
                          description="Just like traditional packages, containers need "
                                      "some 'man page' information about how they are to be used,"
                                      " configured, and integrated into a larger stack.",
                          reference_url="https://fedoraproject.org/wiki/Container:Guidelines#Help_File",
-                         files=['/help.1'],
+                         files=['/help.1', '/README.md'],
                          tags=['filesystem', 'helpfile', 'man'],
                          all_must_be_present=False)
