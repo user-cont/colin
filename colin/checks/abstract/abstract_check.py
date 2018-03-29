e619072cf4f35e317f5fad8954dc7c489cd218b0
lachmanfrantisek <flachman@redhat.com>
2018-03-29 11:28:04 +0200

    Prepare checks for print and json export
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- colin/checks/abstract/abstract_check.py
+++ colin/checks/abstract/abstract_check.py
@@ -13,6 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+import json
+
+from six import iteritems
+
 
 class AbstractCheck(object):
 
@@ -27,3 +31,50 @@ class AbstractCheck(object):
 
     def check(self, target):
         pass
+
+    def __str__(self):
+        return "{}\n" \
+               "   -> {}\n" \
+               "   -> {}\n" \
+               "   -> {}\n" \
+               "   -> {}\n" \
+               "   -> {}\n".format(self.name,
+                                   self.message,
+                                   self.description,
+                                   self.reference_url,
+                                   self.tags,
+                                   self.severity)
+
+    @property
+    def json(self):
+        """
+        Get json representation of the check
+
+        :return: dict (str -> obj)
+        """
+        return {
+            'name': self.name,
+            'message': self.message,
+            'description': self.description,
+            'reference_url': self.reference_url,
+            'tags': self.tags,
+            'severity': self.severity,
+        }
+
+    @staticmethod
+    def json_from_all_checks(checks):
+        result_json = {}
+        for (group, group_checks) in iteritems(checks):
+
+            result_list = []
+            for check in group_checks:
+                result_list.append(check.json)
+
+            result_json[group] = result_list
+        return result_json
+
+    @staticmethod
+    def save_checks_to_json(file, checks):
+        json.dump(obj=AbstractCheck.json_from_all_checks(checks=checks),
+                  fp=file,
+                  indent=4)
