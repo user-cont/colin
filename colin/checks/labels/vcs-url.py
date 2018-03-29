7e33e6d1c5576619027f7cf5111cf18b6aa67254
lachmanfrantisek <flachman@redhat.com>
2018-03-29 11:45:15 +0200

    Add optional labels for redhat
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

---
+++ colin/checks/labels/vcs-url.py
@@ -0,0 +1,14 @@
+from colin.checks.abstract.labels import LabelCheck
+
+
+class VcsUrlLabelCheck(LabelCheck):
+
+    def __init__(self):
+        super().__init__(name="vcs-url_label",
+                         message="Label 'vcs-url' has to be specified.",
+                         description="URL of the version control repository.",
+                         reference_url="https://github.com/projectatomic/ContainerApplicationGenericLabels",
+                         tags=["vcs-url", "vcs", "label", "optional"],
+                         label="vcs-url",
+                         required=True,
+                         value_regex=None)
