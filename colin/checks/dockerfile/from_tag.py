b39061212e93bfc0be28a6443539b190f1b00685
lachmanfrantisek <flachman@redhat.com>
2018-04-13 10:25:59 +0200

    Use new format for ruleset
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- colin/checks/dockerfile/from_tag.py
+++ colin/checks/dockerfile/from_tag.py
@@ -4,11 +4,12 @@ from colin.checks.abstract.dockerfile import InstructionCheck
 class FromTagCheck(InstructionCheck):
 
     def __init__(self):
-        super().__init__(name="is_tag_latest",
+        super().__init__(name="is_tag_not_latest",
                          message="",
                          description="",
                          reference_url="https://docs.docker.com/engine/reference/builder/#from",
                          tags=["from", "dockerfile", "latest"],
                          instruction="FROM",
-                         regex=".*/latest$",
+                         value_regex=".*/latest$",
                          required=False)
+    # TODO: Does not check if there is no tag => use ImageName parsing.
