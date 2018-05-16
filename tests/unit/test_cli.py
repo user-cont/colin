e5b2eee87b45d5fdb17068098d1d55e77906b22a
lachmanfrantisek <flachman@redhat.com>
2018-05-16 14:26:24 +0200

    Correct test output for list-checks
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -60,21 +60,18 @@ def test_list_checks():
    -> The name and email of the maintainer (usually the submitter).
    -> https://fedoraproject.org/wiki/Container:Guidelines#LABELS
    -> maintainer, label, required
-   -> Required: None
 
 from_tag_not_latest
    -> In FROM, tag has to be specified and not 'latest'.
    -> Using the 'latest' tag may cause unpredictable builds.It is recommended that a specific tag is used in the FROM.
    -> https://fedoraproject.org/wiki/Container:Guidelines#FROM
    -> from, dockerfile, baseimage, latest, required
-   -> Required: None
 
 maintainer_deprecated
    -> Dockerfile instruction `MAINTAINER` is deprecated.
    -> Replace with label 'maintainer'.
    -> https://docs.docker.com/engine/reference/builder/#maintainer-deprecated
    -> maintainer, dockerfile, deprecated, required
-   -> Required: None
 
 """
     assert result.exit_code == 0
