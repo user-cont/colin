853b27402bff975edafbbd555d7bfa7ada18ca3d
Jan Ščotka <jscotka@redhat.com>
2018-08-22 11:12:24 +0200

    deprecated labels transformation to FMF format

--- colin/checks/deprecated_labels.py
+++ colin/checks/deprecated_labels.py
@@ -15,94 +15,32 @@
 #
 
 from colin.core.checks.labels import DeprecatedLabelAbstractCheck
+from colin.core.checks.fmf_check import FMFAbstractCheck
 
 
-class ArchitectureLabelCapitalDeprecatedCheck(DeprecatedLabelAbstractCheck):
+class ArchitectureLabelCapitalDeprecatedCheck(FMFAbstractCheck, DeprecatedLabelAbstractCheck):
     name = "architecture_label_capital_deprecated"
 
-    def __init__(self):
-        super(ArchitectureLabelCapitalDeprecatedCheck, self). \
-            __init__(message="Label 'Architecture' is deprecated.",
-                     description="Replace with 'architecture'.",
-                     reference_url="?????",
-                     tags=["architecture", "label", "capital", "deprecated"],
-                     old_label="Architecture",
-                     new_label="architecture")
 
-
-class BZComponentDeprecatedCheck(DeprecatedLabelAbstractCheck):
+class BZComponentDeprecatedCheck(FMFAbstractCheck, DeprecatedLabelAbstractCheck):
     name = "bzcomponent_deprecated"
 
-    def __init__(self):
-        super(BZComponentDeprecatedCheck, self) \
-            .__init__(message="Label 'BZComponent' is deprecated.",
-                      description="Replace with 'com.redhat.component'.",
-                      reference_url="?????",
-                      tags=["com.redhat.component", "bzcomponent", "label", "deprecated"],
-                      old_label="BZComponent",
-                      new_label="com.redhat.component")
+
+class InstallLabelCapitalDeprecatedCheck(FMFAbstractCheck, DeprecatedLabelAbstractCheck):
+    name = "_install_label_capital_deprecated"
 
 
-class InstallLabelCapitalDeprecatedCheck(DeprecatedLabelAbstractCheck):
-    name = "install_label_capital_deprecated"
-
-    def __init__(self):
-        super(InstallLabelCapitalDeprecatedCheck, self) \
-            .__init__(message="Label 'INSTALL' is deprecated.",
-                      description="Replace with 'install'.",
-                      reference_url="?????",
-                      tags=["install", "label", "capital", "deprecated"],
-                      old_label="INSTALL",
-                      new_label="install")
-
-
-class NameLabelCapitalDeprecatedCheck(DeprecatedLabelAbstractCheck):
+class NameLabelCapitalDeprecatedCheck(FMFAbstractCheck, DeprecatedLabelAbstractCheck):
     name = "name_label_capital_deprecated"
 
-    def __init__(self):
-        super(NameLabelCapitalDeprecatedCheck, self) \
-            .__init__(message="Label 'Name' is deprecated.",
-                      description="Replace with 'name'.",
-                      reference_url="?????",
-                      tags=["name", "label", "capital", "deprecated"],
-                      old_label="Name",
-                      new_label="name")
 
-
-class ReleaseLabelCapitalDeprecatedCheck(DeprecatedLabelAbstractCheck):
+class ReleaseLabelCapitalDeprecatedCheck(FMFAbstractCheck, DeprecatedLabelAbstractCheck):
     name = "release_label_capital_deprecated"
 
-    def __init__(self):
-        super(ReleaseLabelCapitalDeprecatedCheck, self) \
-            .__init__(message="Label 'Release' is deprecated.",
-                      description="Replace with 'release'.",
-                      reference_url="?????",
-                      tags=["release", "label", "capital", "deprecated"],
-                      old_label="Release",
-                      new_label="release")
 
-
-class UninstallLabelCapitalDeprecatedCheck(DeprecatedLabelAbstractCheck):
+class UninstallLabelCapitalDeprecatedCheck(FMFAbstractCheck, DeprecatedLabelAbstractCheck):
     name = "uninstall_label_capital_deprecated"
 
-    def __init__(self):
-        super(UninstallLabelCapitalDeprecatedCheck, self) \
-            .__init__(message="Label 'UNINSTALL' is deprecated.",
-                      description="Replace with 'uninstall'.",
-                      reference_url="?????",
-                      tags=["uninstall", "label", "capital", "deprecated"],
-                      old_label="UNINSTALL",
-                      new_label="uninstall")
 
-
-class VersionLabelCapitalDeprecatedCheck(DeprecatedLabelAbstractCheck):
+class VersionLabelCapitalDeprecatedCheck(FMFAbstractCheck, DeprecatedLabelAbstractCheck):
     name = "version_label_capital_deprecated"
-
-    def __init__(self):
-        super(VersionLabelCapitalDeprecatedCheck, self) \
-            .__init__(message="Label 'Version' is deprecated.",
-                      description="Replace with 'version'.",
-                      reference_url="?????",
-                      tags=["version", "label", "capital", "deprecated"],
-                      old_label="Version",
-                      new_label="version")
