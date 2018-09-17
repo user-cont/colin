02efcb334bbafd90f81253d6be95c81d8b1bc0f1
lachmanfrantisek <flachman@redhat.com>
2018-09-17 09:58:16 +0200

    Install xattr on ci setup
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

--- setup-ci.sh
+++ setup-ci.sh
@@ -7,10 +7,10 @@ echo "yum copr enable -y shosca/python"
 yum copr enable -y shosca/python
 
 echo "yum -y install make gcc python36 python36-devel python36-setuptools python36-pip"
-yum -y install make gcc python36 python36-devel python36-setuptools python36-pip
+yum -y install make gcc python36 python36-devel python36-setuptools python36-pip python36-cffi python36-pycparser
 
-echo "pip3 install pytest"
-pip3.6 install pytest
+echo "pip3 install pytest xattr"
+pip3.6 install pytest xattr
 
 echo "yum copr enable -y baude/Upstream_CRIO_Family"
 yum copr enable -y baude/Upstream_CRIO_Family
