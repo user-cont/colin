6d7317168f09dc0613966b4d65b5f5b73010deba
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-02-21 18:03:49 +0200

    Python 3.6 is the minimum
    Co-Authored-By: Eric Brown <ericwb@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
                                            "rulesets/fedora.json"]),
                 ("share/bash-completion/completions/", ["bash-complete/colin"])],
     license='GPLv3+',
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
